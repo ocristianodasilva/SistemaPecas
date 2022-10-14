43b7fe7cffb36dd39889d75c57efbb4cf2dcdd1f
Cristiano da Silva <ocristianodasilvaoficial@gmail.com>
2022-10-14 15:34:11 -0300

    Sistema de Cadastro de Peças Finalizado

---
+++ pecas.js
@@ -0,0 +1,41 @@
+// console.log("Hello World!")
+
+let pesoPeca = 99
+
+if(pesoPeca > 100) {
+
+    console.log("Peso maior que 100 gramas, logo, podemos cadastrar a peça!")
+
+} else {
+    console.log("Peso insulficiente, não é possível cadastrar.")
+}
+
+let numeroPecas = 8
+
+if(numeroPecas < 10) {
+
+    console.log("Ainda há espaço na caixa, podemos cadastrar a peça.")
+
+} else {
+    console.log("Não há espaço disponível na caixa.")
+}
+
+// const teste = 100;
+
+// teste = 500;
+
+// console.log(teste);
+
+let nomePeca = "Disco de Freio"
+
+console.log("O comprimento do nome da peça é:", nomePeca.length)
+
+if(nomePeca.length < 3) {
+
+    console.log("Nome de peça muito curto, não é possível cadastrar.");
+
+} else {
+
+    console.log("Nome de peça adequado, podemos cadastrar.");
+
+}
